1e8af3cba787d7885d1d70b37f244beadbad8801
Simon Steele <simon.steele@mymail.champlain.edu>
2020-09-19 19:57:19 -0400

    Removed calculator and mouse image ref

--- godot-3.2.2-stable/godot-3.2.2-stable/modules/calculator/register_types.h
+++
@@ -1,9 +0,0 @@
-void register_calculator_types();
-void unregister_calculator_types();
-
-/*
-	void register_MODULNAME_types();
-	void unregister_MODULNAME_types();
-
-	MODULNAME ->> insert the name of your module
-*/
