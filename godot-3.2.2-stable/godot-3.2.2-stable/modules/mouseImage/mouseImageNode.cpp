c0ff869d2836ce83b010291888448ee3c6340c4a
Simon Steele <simon.steele@mymail.champlain.edu>
2020-09-19 19:37:42 -0400

    Cleaned out mouse image node so it is readable

--- godot-3.2.2-stable/godot-3.2.2-stable/modules/mouseImage/mouseImageNode.cpp
+++ godot-3.2.2-stable/godot-3.2.2-stable/modules/mouseImage/mouseImageNode.cpp
@@ -3,56 +3,66 @@
 #include "core/bind/core_bind.h"
 #include "scene/main/viewport.h"
 
+// This function is the default constructor for this class
 MouseImageNode::MouseImageNode()
 {
 	canvas = VisualServer::get_singleton()->canvas_item_create();
-	frame = 0;
-	vframes = 1;
-	hframes = 1;
-	centered = true;
 }
 
 //Bind all your methods used in this class
-void MouseImageNode::_bind_methods() {
+void MouseImageNode::_bind_methods()
+{
 	ClassDB::bind_method(D_METHOD("path", "a"), &MouseImageNode::path);
-	ClassDB::bind_method(D_METHOD("location", "a"), &MouseImageNode::location);
-	ClassDB::bind_method(D_METHOD("draw"), &MouseImageNode::draw);
 }
 
 
-//Custom Functions
+// This function sets the image path for the mouse image based on the path string
 void MouseImageNode::path(String a) {
 	imagePath = a;
+	setupNewTexture();
 }
 
-void MouseImageNode::location(Point2 newLocation)
-{
-	iconLocationX = newLocation.x;
-	iconLocationY = newLocation.y;
-}
 
+// This function returns the location of the mouse cursor in screen space
 Point2 MouseImageNode::getMouseLocation()
 {
 	return OS::get_singleton()->get_mouse_position();
 }
 
-void MouseImageNode::draw()
+
+// This function sets up the new image icon based on the image path presented earlier
+void MouseImageNode::setupNewTexture()
 {
-	Ref<Image> img;
-	img.instance();
-	img->load(imagePath);
-	Ref<ImageTexture> newTex;
-	newTex.instance();
-	newTex->create_from_image(img);
-	set_texture(newTex);
+	if (imagePath != "")
+	{
+		Ref<Image> img;
+		img.instance();
+		img->load(imagePath);
+		Ref<ImageTexture> newTex;
+		newTex.instance();
+		newTex->create_from_image(img);
+		set_texture(newTex);
+	}
+	
+}
+
+
+// This function updates the location of the mouse icon based on the location of the mouse cursor
+void MouseImageNode::updateLocation()
+{
 	set_offset(getMouseLocation());
 }
 
+
+// This function gets triggered whenever a notification is recieved
 void MouseImageNode::_notification(int p_what)
 {
 	switch (p_what) {
 
+		// Draw
 		case NOTIFICATION_DRAW: {
+
+			// Draw mouse image if texture is valid
 			if (texture.is_null())
 				return;
 			RID ci = get_canvas_item();
@@ -62,36 +72,34 @@ void MouseImageNode::_notification(int p_what)
 			texture->draw_rect_region(ci, dst_rect, src_rect, Color(1, 1, 1), false, normal_map, filter_clip);
 		} break;
 
+		// Update
 		case NOTIFICATION_PROCESS:
 		{
-			location(getMouseLocation());
+			updateLocation();
 			update();
 		}
 	}
 }
 
+
+// This function gets and sets all of the rectangular regions for drawing
 void MouseImageNode::_get_rects(Rect2 &r_src_rect, Rect2 &r_dst_rect, bool &r_filter_clip) const {
 
 	Rect2 base_rect;
 
-	if (region) {
-		r_filter_clip = region_filter_clip;
-		base_rect = region_rect;
-	} else {
-		r_filter_clip = false;
-		base_rect = Rect2(0, 0, texture->get_width(), texture->get_height());
-	}
+	r_filter_clip = false;
+	base_rect = Rect2(0, 0, texture->get_width(), texture->get_height());
 
-	Size2 frame_size = base_rect.size / Size2(hframes, vframes);
-	Point2 frame_offset = Point2(frame % hframes, frame / hframes);
+	Size2 frame_size = base_rect.size;
+	Point2 frame_offset = Point2(1, 1);
 	frame_offset *= frame_size;
 
 	r_src_rect.size = frame_size;
 	r_src_rect.position = base_rect.position + frame_offset;
 
 	Point2 dest_offset = offset;
-	if (centered)
-		dest_offset -= frame_size / 2;
+	dest_offset -= frame_size / 2;
+
 	if (Engine::get_singleton()->get_use_pixel_snap()) {
 		dest_offset = dest_offset.floor();
 	}
@@ -99,6 +107,8 @@ void MouseImageNode::_get_rects(Rect2 &r_src_rect, Rect2 &r_dst_rect, bool &r_fi
 	r_dst_rect = Rect2(dest_offset, frame_size);
 }
 
+
+// This function sets the texture of the icon and notifies the visual server that it has been changed
 void MouseImageNode::set_texture(const Ref<Texture> &p_texture)
 {
 	if (p_texture == texture)
@@ -112,11 +122,12 @@ void MouseImageNode::set_texture(const Ref<Texture> &p_texture)
 	if (texture.is_valid())
 		texture->connect(CoreStringNames::get_singleton()->changed, this, "_texture_changed");
 
-	update();
 	item_rect_changed();
 	_change_notify("texture");
 }
 
+
+// This function is triggered whenever the texture is changed
 void MouseImageNode::_texture_changed()
 {
 	if (texture.is_valid())
@@ -125,11 +136,15 @@ void MouseImageNode::_texture_changed()
 	}
 }
 
+
+// This function gets the current texture of the icon
 Ref<Texture> MouseImageNode::get_texture() const {
 
 	return texture;
 }
 
+
+// This function sets the offset of the cursor image
 void MouseImageNode::set_offset(const Point2 &p_offset) {
 
 	offset = p_offset;
@@ -137,6 +152,9 @@ void MouseImageNode::set_offset(const Point2 &p_offset) {
 	item_rect_changed();
 	_change_notify("offset");
 }
+
+
+// This function gets the offset of the cursor image
 Point2 MouseImageNode::get_offset() const {
 
 	return offset;
--- godot-3.2.2-stable/godot-3.2.2-stable/modules/mouseImage/mouseImageNode.h
+++ godot-3.2.2-stable/godot-3.2.2-stable/modules/mouseImage/mouseImageNode.h
@@ -6,7 +6,6 @@
 #include "scene/2d/canvas_item.h"
 #include "core/os/input.h"
 #include "core/core_string_names.h"
-#include "core/print_string.h"
 
 using namespace std;
 
@@ -14,34 +13,49 @@ class MouseImageNode : public Node2D {
 	GDCLASS(MouseImageNode, Node2D);
 
 protected:
+  // Update input functions
+  Point2 getMouseLocation();
+  void updateLocation();
+
+  // Private Texture Functions
+  void setupNewTexture();
+
+  // Built-in Node2D functions
   static void _bind_methods();
   void _notification(int p_what);
   virtual void _validate_property(PropertyInfo &property) const {};
-  int iconLocationX;
-  int iconLocationY;
+
+  // Strings
   String imagePath;
+
+  // RID's
   RID canvas;
-  bool region;
-  int frame;
+
+  // Textures
   Ref<Texture> texture;
   Ref<Texture> normal_map;
-  bool centered;
-  Point2 offset;
-  Rect2 region_rect;
-  bool region_filter_clip;
-  int vframes;
-  int hframes;
-public:
-  MouseImageNode();
 
+  // Point2's
+  Point2 offset;
+
+public:
+
+  // Constructor / Destructors
+  MouseImageNode();
+  ~MouseImageNode(){};
+
+  // Path functions
   void path(String a);
-  void location(Point2 newLocation);
-  Point2 getMouseLocation();
-  void draw();
+
+  // Texture functions
   void _texture_changed();
   void set_texture(const Ref<Texture> &p_texture);
   Ref<Texture> get_texture() const;
+
+  // Rect functions
   void _get_rects(Rect2 &r_src_rect, Rect2 &r_dst_rect, bool &r_filter_clip) const;
+
+  // Offset functions
   void set_offset(const Point2 &p_offset);
   Point2 get_offset() const;
 
