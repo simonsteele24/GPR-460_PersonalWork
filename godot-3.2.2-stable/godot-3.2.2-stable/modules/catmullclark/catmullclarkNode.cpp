4daadb718108ab4cd32e09d7f183076c45444fcb
Simon Steele <simon.steele@mymail.champlain.edu>
2020-10-26 22:24:41 -0400

    Added Ashish's sample code to project as baseline for now

--- godot-3.2.2-stable/godot-3.2.2-stable/modules/catmullclark/catmullclarkNode.cpp
+++ godot-3.2.2-stable/godot-3.2.2-stable/modules/catmullclark/catmullclarkNode.cpp
@@ -1,30 +1,164 @@
 #include "catmullclarkNode.h"
 
-CatmullClarkNode::CatmullClarkNode(){}
+CatmullClarkNode::CatmullClarkNode() {
+	numSubdivisions = 0;
+	redraw = true;
+}
 
 //Bind all your methods used in this class
-void CatmullClarkNode::_bind_methods(){
-  ClassDB::bind_method(D_METHOD("add", "a", "b"), &CatmullClarkNode::add);
-  ClassDB::bind_method(D_METHOD("sub", "a", "b"), &CatmullClarkNode::sub);
-  ClassDB::bind_method(D_METHOD("mul", "a", "b"), &CatmullClarkNode::mul);
-  ClassDB::bind_method(D_METHOD("div", "a", "b"), &CatmullClarkNode::div);
-}
-
 
 //Custom Functions
-int CatmullClarkNode::add(int a, int b) {
-  return a+b;
+void CatmullClarkNode::_ready() {
+	numTriangles = 2;
+	set_process(true);
+	set_process_input(true);
+
+	//6 vertices for the two triangles
+	vertices.push_back(Vector2(0, 100));
+	vertices.push_back(Vector2(100, 0));
+	vertices.push_back(Vector2(0, 0));
+	vertices.push_back(Vector2(0, 100));
+	vertices.push_back(Vector2(100, 100));
+	vertices.push_back(Vector2(100, 0));
+
+	colors.append(Color(1, 1, 1));
+	colors.append(Color(0, 1, 0));
+	colors.append(Color(0, 0, 1));
+	colors.append(Color(1, 0, 0));
+	colors.append(Color(0, 0, 0));
+	colors.append(Color(0, 1, 0));
+
+	//Copying the data into another set
+	_vertices.push_back(Vector2(0, 100));
+	_vertices.push_back(Vector2(100, 0));
+	_vertices.push_back(Vector2(0, 0));
+	_vertices.push_back(Vector2(0, 100));
+	_vertices.push_back(Vector2(100, 100));
+	_vertices.push_back(Vector2(100, 0));
+
+	_colors.append(Color(1, 1, 1));
+	_colors.append(Color(0, 1, 0));
+	_colors.append(Color(0, 0, 1));
+	_colors.append(Color(1, 0, 0));
+	_colors.append(Color(0, 0, 0));
+	_colors.append(Color(0, 1, 0));
 }
 
-int CatmullClarkNode::sub(int a, int b) {
-  return a-b;
+void CatmullClarkNode::_update() {
+	int _numTriangles = pow(4, numSubdivisions);
+	if (_numTriangles > 1 && redraw) //no need to enter if we are using just the base mesh
+	{
+		_vertices.resize(0);
+		_colors.resize(0);
+		for (int i = 0; i < vertices.size(); i += 3) {
+			//get the three points for each triangle
+			Vector2 v0 = vertices.get(i);
+			Vector2 v1 = vertices.get(i + 1);
+			Vector2 v2 = vertices.get(i + 2);
+			Color c0 = colors.get(i);
+			Color c1 = colors.get(i + 1);
+			Color c2 = colors.get(i + 2);
+			//find out how many triangles one triangle will subdivide into
+			//you can already see not using an half edge mesh makes it so difficult to do a simple midpoint subdivision
+			float increment = 1.0 / (numSubdivisions + 1);
+			float u = 0;
+			float v = 0; // the algorithm uses the concept of barrycentric coordinates to do the subdivision
+
+			int fcounter = numSubdivisions + 1;
+
+			while (fcounter > 0) {
+				int bcounter = 0;
+				while (bcounter < fcounter) {
+					Vector2 t0 = v0 * (1 - (u + v)) + v1 * u + v * v2;
+					_vertices.push_back(t0);
+					Vector2 t1 = v0 * (1 - (u + increment + v)) + v1 * (u + increment) + v * v2;
+					_vertices.push_back(t1);
+					Vector2 t2 = v0 * (1 - (u + v + increment)) + v1 * u + (v + increment) * v2;
+					_vertices.push_back(t2);
+
+					Color ct0 = c0 * (1 - (u + v)) + c1 * u + c2 * v;
+					_colors.push_back(ct0);
+					Color ct1 = c0 * (1 - (u + increment + v)) + c1 * (u + increment) + c2 * v;
+					_colors.push_back(ct1);
+					Color ct2 = c0 * (1 - (u + v + increment)) + c1 * (v + increment) + c2 * u;
+					_colors.push_back(ct2);
+					u += increment;
+					bcounter++;
+				}
+				//reverse
+				u -= increment;
+				v += increment;
+				bcounter = 0;
+				while (bcounter < fcounter - 1) {
+					Vector2 t0 = v0 * (1 - (u + v)) + v1 * u + v * v2;
+					_vertices.push_back(t0);
+					Vector2 t1 = v0 * (1 - (u - increment + v)) + v1 * (u - increment) + v * v2;
+					_vertices.push_back(t1);
+					Vector2 t2 = v0 * (1 - (u + v - increment)) + v1 * u + (v - increment) * v2;
+					_vertices.push_back(t2);
+
+					Color ct0 = c0 * (1 - (u + v)) + c1 * u + c2 * v;
+					_colors.push_back(ct0);
+					Color ct1 = c0 * (1 - (u - increment + v)) + c1 * (u - increment) + c2 * v;
+					_colors.push_back(ct1);
+					Color ct2 = c0 * (1 - (u + v - increment)) + c1 * (v - increment) + c2 * u;
+					_colors.push_back(ct2);
+					u -= increment;
+					bcounter++;
+				}
+				fcounter--;
+			}
+		}
+	}
 }
 
-int CatmullClarkNode::mul(int a, int b) {
-  return a*b;
+void CatmullClarkNode::_draw() {
+	if (redraw) {
+		Ref<ArrayMesh> a = memnew(ArrayMesh);
+		Array arrays;
+		arrays.resize(ArrayMesh::ARRAY_MAX);
+		arrays[ArrayMesh::ARRAY_VERTEX] = _vertices; // required
+		arrays[ArrayMesh::ARRAY_COLOR] = _colors;
+
+		//optionally u can add texture coordinates and attach a texture from the editor to this arraymesh in the scene
+
+		a->add_surface_from_arrays(Mesh::PRIMITIVE_TRIANGLES, arrays);
+
+		if (this != NULL)
+			this->set_mesh(a);
+		redraw = false;
+	}
 }
 
-int CatmullClarkNode::div(int a, int b) {
-  return a/b;
+void CatmullClarkNode::_input(InputEvent *e) {
+	if (e->is_action_pressed("ui_up")) {
+		numSubdivisions += 1;
+		if (numSubdivisions > 5)
+			numSubdivisions = 1;
+		redraw = true;
+	}
 }
 
+void CatmullClarkNode::_notification(int p_what) {
+
+	switch (p_what) {
+		case NOTIFICATION_PROCESS: {
+			if (Input::get_singleton()->is_action_pressed("ui_up")) {
+				numSubdivisions += 1;
+				if (numSubdivisions > 5)
+					numSubdivisions = 1;
+				redraw = true;
+				SceneTree::get_singleton()->set_input_as_handled();
+			}
+			_update();
+
+			update();
+		} break;
+		case NOTIFICATION_READY: {
+			_ready();
+		} break;
+		case NOTIFICATION_DRAW: {
+			_draw();
+		} break;
+	}
+}
--- godot-3.2.2-stable/godot-3.2.2-stable/modules/catmullclark/catmullclarkNode.h
+++ godot-3.2.2-stable/godot-3.2.2-stable/modules/catmullclark/catmullclarkNode.h
@@ -2,22 +2,36 @@
 #ifndef CATMULLCLARKNODE_H
 #define CATMULLCLARKNODE_H
 
-#include "scene/main/node.h"
+#include "scene/2d/node_2d.h"
 
-class CatmullClarkNode : public Node {
-	GDCLASS(CatmullClarkNode, Node);
+#include "scene/2d/mesh_instance_2d.h"
+#include "scene/resources/mesh.h"
+#include "core/math/triangle_mesh.h"
+#include "core/os/input.h"
+
+class CatmullClarkNode : public MeshInstance2D {
+	GDCLASS(CatmullClarkNode, MeshInstance2D);
 
 protected:
-  static void _bind_methods();
+	int numSubdivisions;
+	int numTriangles;
+	//original
+	PoolVector2Array vertices;
+	PoolColorArray colors;
+
+	//updated
+	PoolVector2Array _vertices;
+	PoolColorArray _colors;
+	bool redraw;
 
 public:
 	CatmullClarkNode();
 
-  int add(int a, int b);
-  int sub(int a, int b);
-  int mul(int a, int b);
-  int div(int a, int b);
-
+	void _notification(int p_what);
+	void _update();
+	void _ready();
+	void _draw();
+	void _input(InputEvent *e);
 };
 
 #endif
